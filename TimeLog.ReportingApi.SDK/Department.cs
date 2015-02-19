5ffb0e69640fb0f6dac036bf0294e5f258d4c7f1
Søren Øxenhave <soeren@oexenhave.dk>
2015-02-19 14:55:39 +0100

    Additional reporting parameter helper classes

---
+++ TimeLog.ReportingApi.SDK/Department.cs
@@ -0,0 +1,13 @@
+﻿namespace TimeLog.ReportingApi.SDK
+{
+    public class Department
+    {
+        public static int All
+        {
+            get
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ TimeLog.ReportingApi.SDK/Project.cs
@@ -0,0 +1,13 @@
+﻿namespace TimeLog.ReportingApi.SDK
+{
+    public class Project
+    {
+        public static int All
+        {
+            get
+            {
+                return 0;
+            }
+        }
+    }
+}
