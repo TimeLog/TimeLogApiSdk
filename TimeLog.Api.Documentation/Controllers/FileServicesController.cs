c1023d4c07cc84936d281334ba8e3674bd7d1a95
Søren Øxenhave <sox@timelog.dk>
2015-07-31 15:48:37 +0200

    Placeholder pages

--- TimeLog.Api.Documentation/Controllers/FileServicesController.cs
+++ TimeLog.Api.Documentation/Controllers/FileServicesController.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-
-namespace TimeLog.Api.Documentation.Controllers
+﻿namespace TimeLog.Api.Documentation.Controllers
 {
+    using System.Web.Mvc;
+
     public class FileServicesController : Controller
     {
         // GET: FileServices
