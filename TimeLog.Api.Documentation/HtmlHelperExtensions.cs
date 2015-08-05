58141c942313ec0daf3112c1d3fbe9c7c574acc9
Søren Øxenhave <soeren@oexenhave.dk>
2015-08-05 08:02:57 +0200

    Menu highlight fix

---
+++ TimeLog.Api.Documentation/HtmlHelperExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace TimeLog.Api.Documentation
+{
+    public static class HtmlHelperExtensions
+    {
+        public static string DetectActiveMenu(this HtmlHelper helper, string controller, bool isDefault = false)
+        {
+            if (HttpContext.Current.Request.Url.AbsolutePath.ToLower().Contains(controller.ToLower()))
+            {
+                return "current";
+            }
+
+            if (isDefault && HttpContext.Current.Request.Url.AbsolutePath.ToLower().Trim('/') == new UrlHelper(HttpContext.Current.Request.RequestContext).Content("~/").ToLower().Trim('/'))
+            {
+                return "current";
+            }
+
+            return string.Empty;
+        }
+    }
+}
