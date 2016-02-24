0ecedbd2f1419edb8338d88f80544a7cd87a45c5
bhl <bhl@timelog.dk>
2016-02-24 16:02:32 +0800

    Add more templates for InsertCustomer and CreateProject.

---
+++ TimeLog.ApiConsoleApp/CreateProjectTransactionalApi2.cs
@@ -0,0 +1,111 @@
+﻿namespace TimeLog.ApiConsoleApp
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using log4net;
+
+    using TimeLog.TransactionalApi.SDK;
+    using TimeLog.TransactionalApi.SDK.ProjectManagementService;
+    using TimeLog.TransactionalApi.SDK.RawHelper;
+
+    using ExecutionStatus = TimeLog.TransactionalApi.SDK.ProjectManagementService.ExecutionStatus;
+
+    /// <summary>
+    /// Template class for consuming the transactional API
+    /// </summary>
+    public class CreateProjectTransactionalApi2
+    {
+        private static readonly ILog Logger = LogManager.GetLogger(typeof(CreateProjectTransactionalApi2));
+
+        public static void Consume()
+        {
+            // For getting the raw XML
+            SecurityHandler.Instance.CollectRawRequestResponse = true;
+            CrmHandler.Instance.CollectRawRequestResponse = true;
+            ProjectManagementHandler.Instance.CollectRawRequestResponse = true;
+
+            IEnumerable<string> messages;
+            if (SecurityHandler.Instance.TryAuthenticate(out messages))
+            {
+                RawMessageHelper.Instance.SaveRecentRequestResponsePair("c:\\temp\\TryAuthenticate.txt");
+                if (Logger.IsInfoEnabled)
+                {
+                    Logger.Info("Sucessfully authenticated on transactional API");
+                }
+
+                var newProject = new Project()
+                {
+                    ID = Guid.NewGuid(),
+                    AccountManagerID = 384,
+                    ProjectManagerID = 384,
+                    TypeID = 247,
+                    StartDate = DateTime.Today,
+                    StageID = 2,
+                    Name = "Projekt PivotPoint",
+                    CustomerID = 707, //Original value 709 does not exist
+                    Action = DataAction.Created,
+                    BudgetAmountExpenses = 0,
+                    BudgetAmountTravelExpenses = 0,
+                    BudgetWorkAmountFixedPriceProject = 0,
+                    BudgetWorkAmountFixedPriceTasks = 0,
+                    BudgetWorkAmountTimeAndMaterial = 0,
+                    BudgetWorkHoursFixedPriceProject = 0,
+                    BudgetWorkHoursFixedPriceTasks = 0,
+                    BudgetWorkHoursTimeAndMaterial = 0,
+                    CategoryID = 3,
+                    CurrencyID = 35,
+                    DepartmentHandledByID = 0,
+                    DepartmentOrderedByID = 0,
+                    Description = string.Empty,
+                    EndDate = DateTime.Today.AddDays(30),
+                    ExchangeRate = 1,
+                    No = "42",
+                    PriceListID = 1,
+                    PriceGroupID = 1,
+                    PurchaseOrderNumber = "42"
+                };
+
+                var createProjectResult = ProjectManagementHandler.Instance.ProjectManagementClient.CreateProject(newProject,
+                    ProjectManagementHandler.Instance.Token);
+
+
+                RawMessageHelper.Instance.SaveRecentRequestResponsePair("c:\\temp\\CreateProject.txt");
+                if (createProjectResult.ResponseState == ExecutionStatus.Success)
+                {
+                    var project = createProjectResult.Return.FirstOrDefault();
+                    if (project != null)
+                    {
+
+                    }
+                    else
+                    {
+                        if (Logger.IsWarnEnabled)
+                        {
+                            Logger.Warn("No project found");
+                        }
+                    }
+                }
+                else
+                {
+                    foreach (var apiMessage in createProjectResult.Messages)
+                    {
+                        if (Logger.IsErrorEnabled)
+                        {
+                            Logger.Error(apiMessage.Message);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                if (Logger.IsWarnEnabled)
+                {
+                    Logger.Warn("Failed to authenticate to transactional API");
+                    Logger.Warn(string.Join(",", messages));
+                }
+            }
+        }
+    }
+}
---
+++ TimeLog.ApiConsoleApp/InsertCustomerTest.cs
@@ -0,0 +1,100 @@
+﻿namespace TimeLog.ApiConsoleApp
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using log4net;
+
+    using TimeLog.TransactionalApi.SDK;
+    using TimeLog.TransactionalApi.SDK.CrmService;
+    using TimeLog.TransactionalApi.SDK.ProjectManagementService;
+    using TimeLog.TransactionalApi.SDK.RawHelper;
+
+    using ExecutionStatus = TimeLog.TransactionalApi.SDK.ProjectManagementService.ExecutionStatus;
+
+    /// <summary>
+    /// Template class for consuming the transactional API
+    /// </summary>
+    public class InsertCustomerTest
+    {
+        private static readonly ILog Logger = LogManager.GetLogger(typeof(InsertCustomerTest));
+
+        public static void Consume()
+        {
+            // For getting the raw XML
+            SecurityHandler.Instance.CollectRawRequestResponse = true;
+            CrmHandler.Instance.CollectRawRequestResponse = true;
+            ProjectManagementHandler.Instance.CollectRawRequestResponse = true;
+
+            IEnumerable<string> messages;
+            if (SecurityHandler.Instance.TryAuthenticate(out messages))
+            {
+                RawMessageHelper.Instance.SaveRecentRequestResponsePair("c:\\temp\\TryAuthenticate.txt");
+                if (Logger.IsInfoEnabled)
+                {
+                    Logger.Info("Sucessfully authenticated on transactional API");
+                }
+
+                // Get a customer
+
+                var newContactHeader = new ContactHeader();
+                newContactHeader.FirstName = "FirstTest";
+                newContactHeader.LastName = "LastTest";
+                newContactHeader.GUID = Guid.NewGuid();
+
+                var newCustomer = new Customer()
+                {
+                    Name = "NewCustomerDDDDD",
+                    AccountNo = String.Empty,
+                    Action = TransactionalApi.SDK.CrmService.DataAction.Created,
+                    Address = new Address() { Address1 = "123", Country = "Denmark", City = "Copenhagen", State = "dunno", ZipCode = "68000"},
+                    CalculateVAT = true,
+                    Contacts = new ContactHeader[] { newContactHeader },
+                    CreditorID = string.Empty,
+                    Currency = "DKK",
+                    DefaultDiscountPercent = 2,
+                    Status = "50. Kunde 123456",
+                    ID = 11078
+                };
+
+                var customersResult = CrmHandler.Instance.CrmClient.InsertCustomer(newCustomer, 2,
+                    CrmHandler.Instance.Token);
+                RawMessageHelper.Instance.SaveRecentRequestResponsePair("c:\\temp\\InsertCustomer.txt");
+                if (customersResult.ResponseState == TransactionalApi.SDK.CrmService.ExecutionStatus.Success)
+                {
+                    var customer = customersResult.Return.FirstOrDefault();
+                    if (customer != null)
+                    {
+                        
+                    }
+                    else
+                    {
+                        if (Logger.IsWarnEnabled)
+                        {
+                            Logger.Warn("No customer found");
+                        }
+                    }
+                }
+                else
+                {
+                    foreach (var apiMessage in customersResult.Messages)
+                    {
+                        if (Logger.IsErrorEnabled)
+                        {
+                            Logger.Error(apiMessage.Message);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                if (Logger.IsWarnEnabled)
+                {
+                    Logger.Warn("Failed to authenticate to transactional API");
+                    Logger.Warn(string.Join(",", messages));
+                }
+            }
+        }
+    }
+}
