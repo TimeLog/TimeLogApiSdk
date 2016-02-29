a329bbdcadb9415d7bb527522bd225c8649ce5ee
Søren Øxenhave <sox@timelog.dk>
2016-02-29 21:43:42 +0100

    Creating placeholder pages for new documentation

---
+++ TimeLog.Api.Documentation/Controllers/FinancialIntegrationController.cs
@@ -0,0 +1,53 @@
+﻿namespace TimeLog.Api.Documentation.Controllers
+{
+    using System.Web.Mvc;
+
+    public class FinancialIntegrationController : Controller
+    {
+        // GET: FinancialIntegration
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult DynamicsNav()
+        {
+            return View("Index");
+        }
+
+        public ActionResult DynamicsAx()
+        {
+            return View("Index");
+        }
+
+        public ActionResult ExactOnline()
+        {
+            return View("Index");
+        }
+
+        public ActionResult VismaAdministration()
+        {
+            return View("Index");
+        }
+
+        public ActionResult Economic()
+        {
+            return View("Index");
+        }
+
+        public ActionResult Flows()
+        {
+            return View("Index");
+        }
+
+        public ActionResult Tests()
+        {
+            return View("Index");
+        }
+
+        public ActionResult Install()
+        {
+            return View("Index");
+        }
+    }
+}
