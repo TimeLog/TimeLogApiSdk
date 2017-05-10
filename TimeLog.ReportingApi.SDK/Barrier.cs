38df145e6d124c58f8ee5b10b3f33594c567aad7
Søren Øxenhave <soeren@oexenhave.dk>
2017-05-10 15:38:29 +0200

    Added GetOpportunitiesRaw to reporting API exporter

---
+++ TimeLog.ReportingApi.SDK/Barrier.cs
@@ -0,0 +1,13 @@
+﻿namespace TimeLog.ReportingApi.SDK
+{
+    public class Barrier
+    {
+        public static int All
+        {
+            get
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ TimeLog.ReportingApi.SDK/Opportunity.cs
@@ -0,0 +1,44 @@
+﻿namespace TimeLog.ReportingApi.SDK
+{
+    using System;
+
+    public class Opportunity
+    {
+        /// <summary>
+        /// Gets or sets the identifier
+        /// </summary>
+        public int Id { get; set; }
+
+        public int CustomerId { get; set; }
+
+        public int ContactId { get; set; }
+
+        public string Subject { get; set; }
+
+        public int OpportunityTypeId { get; set; }
+
+        public int EmployeeId { get; set; }
+
+        public int BarrierId { get; set; }
+
+        public double Potential { get; set; }
+
+        public int ForecastRate { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public int CreatedBy { get; set; }
+
+        public DateTime LastModified { get; set; }
+
+        public int LastModifiedBy { get; set; }
+
+        public DateTime QuotationSent { get; set; }
+
+        public string Quotation { get; set; }
+
+        public int CurrencyId { get; set; }
+    }
+}
