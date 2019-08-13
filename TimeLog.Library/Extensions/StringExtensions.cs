5a3ca5d2b12adbbba99e04eb3ec43c598a5ab5c5
Søren Øxenhave <sox@timelog.dk>
2019-08-13 11:09:00 +0200

    Add a little nice extension method

---
+++ TimeLog.Library/Extensions/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TimeLog.Library.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string Truncate(this string s, int length)
+        {
+            if (s == null)
+            {
+                return string.Empty;
+            }
+
+            return s.Substring(0, Math.Min(s.Length, length));
+        }
+    }
+}
