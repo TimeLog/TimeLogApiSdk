a45ce1695b243303e4abfdb448225dda29bed654
Søren Øxenhave <sox@timelog.dk>
2021-03-24 22:33:49 +0100

    Add initial attempt at upgrading the API website to .NET core

---
+++ TimeLog.Api.Core.Documentation/Controllers/GettingStartedController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TimeLog.Api.Core.Documentation.Controllers
+{
+    public class GettingStartedController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
