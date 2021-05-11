d79a1f4425e832ab98e887e3109230a4e3653796
Anders Pedersen <anderspedersen86@gmail.com>
2021-05-11 11:49:04 +0200

    Docs/fix mobile nav toggle (#42)
    * add toggle functionality to mobile nav
    
    * add js for toggling submenus
    
    Co-authored-by: Anders Pedersen <apd@timelog.dk>

--- TimeLog.Api.Core.Documentation/wwwroot/js/site.js
+++ TimeLog.Api.Core.Documentation/wwwroot/js/site.js
@@ -9,3 +9,50 @@ if (mobileNavButton) {
     document.body.classList.toggle("menu-toggled");
   });
 }
+
+// Handle the toggling of sub menus
+var mq = window.matchMedia("(max-width: 799px)");
+var topLevelLinks = Array.from(
+  document.getElementsByClassName("top-level__link")
+);
+
+function clickHandler(eventObj) {
+  eventObj.preventDefault();
+  // This will not work in IE11
+  var parent = eventObj.target.closest(".top-level__item");
+  parent.classList.toggle("active");
+}
+
+if (mq.matches && topLevelLinks) {
+  topLevelLinks.forEach(function (item) {
+    item.addEventListener("click", clickHandler);
+  });
+}
+
+// Setup a timer
+var timeout;
+
+// Listen for resize events
+window.addEventListener(
+  "resize",
+  function (event) {
+    // If there's a timer, cancel it
+    if (timeout) {
+      window.cancelAnimationFrame(timeout);
+    }
+
+    // Setup the new requestAnimationFrame()
+    timeout = window.requestAnimationFrame(function () {
+      if (mq.matches && topLevelLinks) {
+        topLevelLinks.forEach(function (item) {
+          item.addEventListener("click", clickHandler);
+        });
+      } else {
+        topLevelLinks.forEach(function (item) {
+          item.removeEventListener("click", clickHandler);
+        });
+      }
+    });
+  },
+  false
+);
